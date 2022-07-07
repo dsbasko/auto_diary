76ec8f656aec39308f28a341eb04335f99b91756
Dmitriy Basenko <d.basenko.it@gmail.com>
2022-07-07 12:22:36 +0500

    0.0.1
    * basic configure is done
    * сonfigured frontend

---
+++ apps/frontend/app/config/tailwind/color.js
@@ -0,0 +1,17 @@
+const TailwindColors = {
+  primary: '#297AFF',
+  text: {
+    white: '#ffffff',
+    gray: '#84878D',
+  },
+  bg: {
+    main: '#1E1E1E',
+    lighten: '#22242E',
+    darken: '#0A0B0D',
+  },
+  input: {
+    main: '#41454D',
+  },
+};
+
+module.exports = TailwindColors;
